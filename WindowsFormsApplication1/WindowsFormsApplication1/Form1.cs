1cf5019aaebe5778410592f29499f3931116de4e
Dejan Arsikj <dejan_arsic@live.com>
2013-04-28 22:17:03 +0200

    prvaKonecna

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -16,5 +16,28 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = this.CreateGraphics();
+            g.FillEllipse(new SolidBrush(Color.Gray), new Rectangle(50, 50, 50, 10));
+            g.FillRectangle(new SolidBrush(Color.Gray), new Rectangle(55, 55, 40, 7));
+            Point[] points = new Point[4];
+            points[0] = new Point(55, 62);
+            points[3] = new Point(95, 62);
+            points[1] = new Point(40, 82);
+            points[2] = new Point(110, 82);
+            g.FillPolygon(new SolidBrush(Color.Green), points);
+            g.FillRectangle(new SolidBrush(Color.Black), new Rectangle(40, 82, 70, 4));
+            points[0] = new Point(39, 86);
+            points[3] = new Point(30, 106);
+            points[1] = new Point(111, 86);
+            points[2] = new Point(120, 106);
+            g.FillPolygon(new SolidBrush(Color.Green), points);
+            g.FillRectangle(new SolidBrush(Color.Green), new Rectangle(30, 106, 90, 60));
+            g.FillRectangle(new SolidBrush(Color.Black), new Rectangle(30, 166, 90, 22));
+            g.DrawString("Heinken", new Font("Arial", 16), new SolidBrush(Color.White), new Rectangle(30, 166, 90, 20));
+            g.FillRectangle(new SolidBrush(Color.Green), new Rectangle(30, 188, 90, 60));
+        }
     }
 }
--- WindowsFormsApplication1/WindowsFormsApplication1/Properties/AssemblyInfo.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("18ac544c-21b9-486b-829f-10c2461690b0")]
+[assembly: Guid("c2d8ab01-059d-4c54-9ea1-75867ffbfb37")]
 
 // Version information for an assembly consists of the following four values:
 //
